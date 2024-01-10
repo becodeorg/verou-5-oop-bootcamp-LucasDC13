4de02c586c60b7891344fde893491107a0bd8877
LucasDC13 <lucas_de_corte@hotmail.com>
2024-01-10 09:34:45 +0100

    Merge index and view

--- usecase1/index.php
+++ usecase1/index.php
@@ -34,5 +34,33 @@ $items = [$item1, $item2, $item3];
 $cartTotal = 0;
 $tax;
 $priceMod;
+?>
 
-require "view.php";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Drinks</title>
+</head>
+<body>
+    <h1>Order:</h1>
+    <?php foreach ($items as $item): ?>
+        <?php if ($item->getCategory() === 'food') {
+            $tax = 0.06;
+            $priceMod = 0.5;
+            echo "<p><strong>Fruit discount applied!</strong></p>";
+        } else if ($item->getCategory() === 'drinks') {
+            $tax = 0.21;
+            $priceMod = 1;
+        }
+        $itemTotal = $item->getQuantity() * $item->getPrice() * $priceMod;
+        echo "<p>" . $item->getQuantity() . "x " . $item->getName() . " totals: €" . $itemTotal . "</p>";
+        echo "<p> Item tax = €" . $itemTotal*$tax . "</p>";
+        $cartTotal += $itemTotal; ?>
+        <hr>
+    <?php endforeach; 
+    echo "<h2> Total: €" . $cartTotal;
+    ?>
+</body>
+</html>
